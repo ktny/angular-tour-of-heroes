cf6124f2e24db9c191a08b85a59c1e29adb6b0b8
ktny <katsunori.t10@gmail.com>
2021-04-29 21:42:26 +0900

    :%s/heros/heroes/g

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HerosComponent } from "./heros/heros.component";
+import { HeroesComponent } from "./heroes/heroes.component";
 
 @NgModule({
-  declarations: [AppComponent, HerosComponent],
+  declarations: [AppComponent, HeroesComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
